3b98d5560c3cbb6b286db67badb7670eb249372f
BigFaceMaster <BigFaceMaster@gmail.com>
2020-06-23 15:45:14 +0800

    add file

---
+++ src/welcome.ts
@@ -0,0 +1,13 @@
+import { serve } from 'https://deno.land/std/http/server.ts'
+
+const s = serve({
+    port: 8000
+})
+
+console.log("http://localhost:8000/")
+
+for await (const req of s) {
+    req.respond({
+        body: "hello world\n"
+    })
+}
